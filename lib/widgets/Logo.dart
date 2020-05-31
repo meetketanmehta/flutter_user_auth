3ce152ed652ab9a4ee871217ac66b519b750466f
Meet Mehta <meet.1@iitj.ac.in>
2020-05-31 23:05:32 +0530

    Changed font size of logo

--- lib/widgets/Logo.dart
+++ lib/widgets/Logo.dart
@@ -10,18 +10,18 @@ class Logo extends StatelessWidget {
           text: 'p',
           style: GoogleFonts.portLligatSans(
             textStyle: Theme.of(context).textTheme.display1,
-            fontSize: 30,
+            fontSize: 40,
             fontWeight: FontWeight.w700,
             color: Color(0xffe46b10),
           ),
           children: [
             TextSpan(
               text: 'iz',
-              style: TextStyle(color: Colors.black, fontSize: 30),
+              style: TextStyle(color: Colors.black, fontSize: 40),
             ),
             TextSpan(
               text: 'on',
-              style: TextStyle(color: Color(0xffe46b10), fontSize: 30),
+              style: TextStyle(color: Color(0xffe46b10), fontSize: 40),
             ),
           ]),
     );
